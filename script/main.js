694749160c950113a30a93b8208a903c0975565c
DimaWebFrontEnd <129187393+DimaWebFrontEnd@users.noreply.github.com>
2024-05-09 20:21:20 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -4,26 +4,27 @@ const burgerBtn = document.querySelector(".bi-list");
 const nav = document.querySelector("nav");
 const menu = document.querySelector("ul");
 const closeMenu = document.querySelector(".bi-x");
-
+const html = document.querySelector("html");
 
 burgerBtn.addEventListener("click", event => {
-   nav.classList.add("active")
-   menu.classList.add("active")
+   nav.classList.add("active");
+   menu.classList.add("active");
    infoContent.classList.remove("active");
    infoDelivery.classList.remove("active");
+   html.style.overflow = "hidden";
 })
 
 closeMenu.addEventListener("click", event => {
    nav.classList.remove("active")
    menu.classList.remove("active")
-   
+   html.style.overflow = "auto";
 })
 
 nav.addEventListener("click", event => {
    if (event.target === nav) {
       nav.classList.remove("active")
       menu.classList.remove("active")
-      
+      html.style.overflow = "auto";
    }
 })
 
@@ -36,9 +37,11 @@ const infoDelivery = document.querySelector(".delivery");
 const arrowDown = document.querySelector(".bi-chevron-down");
 const arrowUp = document.querySelector(".bi-chevron-up");
 
+
 infoBtn.addEventListener("click", event => {
    infoContent.classList.toggle("active");
    infoDelivery.classList.toggle("active");
    arrowUp.classList.toggle("active");
    arrowDown.classList.toggle("notactive");
 })
+
